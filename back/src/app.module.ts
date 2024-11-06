a7882d85645dd33458d56c12fad8687ac77e60d6
Lucas Fava <lucasfava86@gmail.com>
2024-11-06 19:26:23 -0300

    tabla multimedia

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MultimediaModule } from './multimedia/multimedia.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root123',
+      database: 'eseyoDB',
+      synchronize: false,
+      entities: ["dist/**/**.entity{.ts,.js}"],
+      logging: 'all',
+    }),
+    MultimediaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ back/src/multimedia/entities/multimedia.entity.ts
@@ -0,0 +1,42 @@
+import { Column, PrimaryGeneratedColumn, Entity, ManyToOne, JoinColumn } from "typeorm";
+
+@Entity('multimedia')
+export class Multimedia {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: 'bigint',
+  })
+  private id: number;
+
+  @Column({
+    name: 'linkMultimedia',
+    type: 'varchar',
+    precision: 500,
+  })
+  private linkMultimedia: string;
+
+  @ManyToOne(() => Campania, (campania) => campania.multimedia)
+  @JoinColumn({
+    name: 'id',
+    foreignKeyConstraintName: 'FK_campaniaMultimedia',
+  })
+  public campania: Campania;
+
+
+  constructor(id: number, linkMultimedia: string) {
+    this.id = id;
+
+    this.linkMultimedia = linkMultimedia;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getLinkMultimedia(): string {
+    return this.linkMultimedia;
+  }
+  public setLinkMultimedia(linkMultimedia: string) {
+    this.linkMultimedia = linkMultimedia;
+  }
+}
