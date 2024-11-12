3611d95455be0faa2f000d89b2858f11873238be
planeslorena <planeslorena@gmail.com>
2024-11-12 19:39:10 -0300

    getEquipo listo

--- back/src/equipo/equipo.controller.ts
+++ back/src/equipo/equipo.controller.ts
@@ -6,28 +6,9 @@ import { IEquipo } from './model/iEquipo';
 export class EquipoController {
   constructor(private readonly equipoService: EquipoService) {}
 
-  @Post()
-  create(@Body() createEquipoDto: IEquipo) {
-    return this.equipoService.create(createEquipoDto);
-  }
-
   @Get()
-  findAll() {
-    return this.equipoService.findAll();
+  async findAll():Promise<IEquipo[]> {
+    return await this.equipoService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.equipoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEquipoDto: IEquipo) {
-    return this.equipoService.update(+id, updateEquipoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.equipoService.remove(+id);
-  }
 }
--- back/src/equipo/equipo.module.ts
+++ back/src/equipo/equipo.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EquipoService } from './equipo.service';
 import { EquipoController } from './equipo.controller';
+import { Equipo } from './entities/equipo.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Equipo])],
   controllers: [EquipoController],
   providers: [EquipoService],
 })
